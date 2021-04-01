4ec6ebcb9cdb08269ed0263063e4404cd5f1b431
HLXY <hlxy420@gmail.com>
2021-04-01 19:58:53 +0800

    Make the simple Signup components.

--- src/signup/signup.controller.ts
+++ src/signup/signup.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Query, Res } from '@nestjs/common';
+import { SignupService } from './signup.service';
+import { Respond } from '../interfaces/respond.interface';
 
 @Controller('signup')
-export class SignupController {}
+export class SignupController {
+  constructor(private readonly signupService: SignupService) {}
+
+  /**
+   * 使用Post注册并跳转页面
+   * @param params 传输参数
+   * @param res 用于重定向和返回的原件
+   */
+  @Post()
+  async Signup(@Query() params, @Res() res): Promise<void> {
+    const result: Respond = await this.signupService.Signup(params);
+    if (result.statusCode === 200) {
+      res
+        .status(302)
+        .redirect(params.service + '?ST=' + result.data.ServerTicket);
+    }
+    res.send(result);
+  }
+}
--- src/signup/signup.service.ts
+++ src/signup/signup.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Respond } from '../interfaces/respond.interface';
+import { Repository, getConnection } from 'typeorm';
+import { Account } from '../entity/account.entity';
 
 @Injectable()
-export class SignupService {}
+export class SignupService {
+  constructor(
+    @InjectRepository(Account)
+    private readonly AccountRepo: Repository<Account>,
+  ) {}
+
+  /**
+   * 注册函数
+   * @param params: {
+   *   username
+   *   nickname
+   *   password
+   *   email
+   *   description
+   * }
+   *
+   * @return statusCode
+   * @return message
+   */
+  async Signup(params): Promise<Respond> {
+    const username = params.username;
+    const nickname = params.nickname;
+    const password = params.password;
+    const email = params.email;
+    const description = params.description;
+
+    await getConnection()
+      .createQueryBuilder()
+      .insert()
+      .into(Account)
+      .values([
+        {
+          username: username,
+          nickname: nickname,
+          password: password,
+          email: email,
+          description: description,
+        },
+      ])
+      .execute();
+
+    return {
+      statusCode: 302,
+      message: 'helloWorld',
+    };
+  }
+}
